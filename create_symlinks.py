a363f80327c2d12e2851b36a9dfd8ed46a5ca265
Nicholas Bishop <nicholasbishop@gmail.com>
2014-09-03 15:42:14 -0400

    Set up scripts for creating symlinks and building
    The main change here is to run a pre-install script to set up
    symlinks, needed because npm doesn't install symlinks even if they're
    present in the git repository.
    
    Also kept the install script to "node-gyp rebuild". It sure doesn't
    seem like this should be necessary (according to the documentation
    node-gyp should run if the .gyp file is present), but it seems like
    it's not getting built on Heroku without this explicit script set.

---
+++ create_symlinks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# npm will not install symlinks that are part of the git repository,
+# so create them manually as a pre-install step instead
+
+import os
+
+def create_symlink(link_to, file_path):
+    print('Creating symlink {} -> {}'.format(file_path, link_to))
+    os.symlink(link_to, file_path)
+
+create_symlink('libglapi.so.0.0.0', 'mesa/lib/libglapi.so.0')
+create_symlink('libglapi.so.0.0.0', 'mesa/lib/libglapi.so')
+
+create_symlink('libOSMesa.so.8.0.0', 'mesa/lib/libOSMesa.so.8')
+create_symlink('libOSMesa.so.8.0.0', 'mesa/lib/libOSMesa.so')
