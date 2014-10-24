a9e43bd014762e3e1db4b48e57a80efd1b137cff
hamham91 <hamham91@gmail.com>
2014-10-24 14:14:06 -0400

    Update create_symlinks.py

--- create_symlinks.py
+++ create_symlinks.py
@@ -3,14 +3,12 @@
 # npm will not install symlinks that are part of the git repository,
 # so create them manually as a pre-install step instead
 
-from subprocess import call
 import os
 
 def create_symlink(link_to, file_path):
     print('Creating symlink {} -> {}'.format(file_path, link_to))
     os.symlink(link_to, file_path)
 
-call(["env"]);
 create_symlink('libglapi.so.0.0.0', 'mesa/lib/libglapi.so.0')
 create_symlink('libglapi.so.0.0.0', 'mesa/lib/libglapi.so')
 
