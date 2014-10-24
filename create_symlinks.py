e53415d35b9b0d0a9b2e80264bccf5e23f36eff9
Lars Hamre <hamham91@gmail.com>
2014-10-24 11:42:21 -0400

    Debugging.

--- create_symlinks.py
+++ create_symlinks.py
@@ -3,9 +3,13 @@
 # npm will not install symlinks that are part of the git repository,
 # so create them manually as a pre-install step instead
 
+from subprocess import call
 import os
 
 def create_symlink(link_to, file_path):
+    print("LOOK AT ME");
+    call(["pwd"]);
+    call(["find", "../.."])
     print('Creating symlink {} -> {}'.format(file_path, link_to))
     os.symlink(link_to, file_path)
 
